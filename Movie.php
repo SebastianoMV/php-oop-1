1dd0860ae93e84124e4c6d537d065eb46cbd608b
SebastianoMV <sebastianovisco@gmail.com>
2022-06-17 16:25:12 +0200

    cunstruct

--- Movie.php
+++ Movie.php
@@ -5,18 +5,16 @@ class Movie{
   public $title;
   public $director;
 
-  
+  public function __construct($_title, $_director){
+    $this->title = $_title;
+    $this->director = $_director;
+  }
 
 }
 
-$movie1 = new Movie();
-$movie2 = new Movie();
+$movie1 = new Movie("Le follie dell'imperatore","Mark Dindal");
+$movie2 = new Movie("Il signore degli anelli","Peter Jackson");
 
-$movie1->title = "Le follie dell'imperatore";
-$movie1->director = "Mark Dindal";
-
-$movie2->title = "Il signore degli anelli";
-$movie2->director = "Peter Jackson";
 
 var_dump($movie1);
 var_dump($movie2);
